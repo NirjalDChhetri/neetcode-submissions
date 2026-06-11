04c86dceb1bf55047885f923ea9c890c7fd4b99c
Nirjal D. Chhetri <nirjald3@gmail.com>
2026-06-12 00:20:20 +0545

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let prefix = "";
+
+        for (let i = 0; i < strs[0].length; i++) {
+            const currentChar = strs[0][i];
+
+            for (let j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length || strs[j][i] !== currentChar) {
+                    return prefix;
+                }
+            }
+            prefix += currentChar;
+        }
+        return prefix;
+    }
+}
